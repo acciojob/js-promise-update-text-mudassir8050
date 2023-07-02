4378c4d27bef6234e069abc4eaf4f64c4e943c1f
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-07-03 00:11:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function delayHello() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+// Update the text of an HTML element with ID "output"
+function updateOutputText() {
+  const outputElement = document.getElementById('output');
+
+  delayHello().then((result) => {
+    outputElement.textContent = result;
+  });
+}
